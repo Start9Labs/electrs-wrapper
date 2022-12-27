01829a7bd57e98b828023a25798befe135c65edd
gStart9 <george@start9labs.com>
2022-12-27 13:43:30 -0700

    Rearrange some check-synced.sh stuff

--- check-synced.sh
+++ check-synced.sh
@@ -30,12 +30,15 @@ if [ "$b_block_count" = "null" ]; then
     exit 60
 else
     features_res=$(echo '{"jsonrpc": "2.0", "method": "server.features", "params": ["", "1.4"], "id": 0}' | netcat -q 1 127.0.0.1 50001)
-    synced_height=$(curl -sS localhost:4224 | grep index_height | grep tip | awk '{ print $NF }')
+    curl_res=$(curl -sS localhost:4224)
     error_code=$?
+    
     if [[ $error_code -ne 0 ]]; then
         echo $synced_height >&2
         exit $error_code
     fi
+
+    synced_height=$(echo -e "$curl_res" | grep index_height | grep tip | awk '{ print $NF }')
 
     if [[ $synced_height -lt $b_block_count ]]; then
         echo "Catching up to blocks from bitcoind. This should take at most a day. Progress: $synced_height of $b_block_count blocks" >&2
