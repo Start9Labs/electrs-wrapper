734ee7af439c5a997bfb50b9781a2e4dc99f0cef
Tim A <41004+takinbo@users.noreply.github.com>
2025-01-26 10:45:59 +0000

    added testnet4 support and restored btc-rpc-proxy support

--- scripts/services/setConfig.ts
+++ scripts/services/setConfig.ts
@@ -1,3 +1,24 @@
 import { compat, types as T } from "../deps.ts";
 
-export const setConfig: T.ExpectedExports.setConfig = compat.setConfig;
+// deno-lint-ignore require-await
+export const setConfig: T.ExpectedExports.setConfig = async (
+  effects: T.Effects,
+  newConfig: T.Config,
+) => {
+  // deno-lint-ignore no-explicit-any
+  const dependsOnBitcoind: { [key: string]: string[] } =
+    (newConfig as any)?.bitcoind?.type === 'bitcoind'
+      ? { bitcoind: ['synced'] }
+      : {}
+
+  // deno-lint-ignore no-explicit-any
+  const dependsOnBitcoindTestnet: { [key: string]: string[] } =
+    (newConfig as any)?.bitcoind?.type === 'bitcoind-testnet'
+      ? { 'bitcoind-testnet': ['synced'] }
+      : {}
+
+  return compat.setConfig(effects, newConfig, {
+    ...dependsOnBitcoind,
+    ...dependsOnBitcoindTestnet,
+  })
+}
